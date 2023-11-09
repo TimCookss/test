6d4551986e09f463a49fb172ab56a31b09ee7625
TimCookss <148066348+TimCookss@users.noreply.github.com>
2023-11-09 14:29:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oF9gp6k_qFn38xWUDK-lUbWJLriQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '34Kioo-F8PnS4b8gSZwCyx8ozTE0e0n0lNpem-O1Fsw',
+      useTemplateId: 'Wnt8zE4DlMwub3h3GPeETTHPJTI7_6Z6LHgY-tzCwzA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
        horoscopeDate: '08-09',
       festivals: [
