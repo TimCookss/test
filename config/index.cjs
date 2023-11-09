bad2c529ab20523dc220ef522ab1c6e619f4cdee
TimCookss <148066348+TimCookss@users.noreply.github.com>
2023-11-09 15:07:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'h',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oF9gp6k_qFn38xWUDK-lUbWJLriQ',
+      id: 'oF9gp6leupKEstK8e18Br-_qOLuk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'OUMM3q-aE5Y0qRGSmAqyOxa--y82tQ6o-Irp85dljIc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
